80a4f093424f1aa249e99eef967e9684bf9a12c3
Kostas Aronis <k.aronis@dotbydot.gr>
2019-07-15 09:51:31 +0300

    Update PanaceaServicesExtensions.cs

--- src/Panacea.Modularity.Relays/PanaceaServicesExtensions.cs
+++ src/Panacea.Modularity.Relays/PanaceaServicesExtensions.cs
@@ -9,7 +9,7 @@ namespace Panacea.Modularity.Relays
 {
     public static class PanaceaServicesExtensions
     {
-        public static bool GetRelayManager(this PanaceaServices core, out IRelayManager relay)
+        public static bool TryGetRelayManager(this PanaceaServices core, out IRelayManager relay)
         {
             relay = core.PluginLoader.GetPlugins<IRelayManagerPlugin>().FirstOrDefault()?.GetRelayManager();
             return relay != null;
